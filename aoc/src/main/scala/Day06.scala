e510e323ffe1de28861205c99da58df90d7cf128
Denise Rijborz <denise.rijborz@dhl.com>
2022-12-06 11:03:42 +0100

    day6 better

--- aoc/src/main/scala/Day06.scala
+++ aoc/src/main/scala/Day06.scala
@@ -13,11 +13,19 @@ object Day06 extends App:
     Source
       .fromResource(s"input$day.txt").getLines.mkString
 
-  val fourChar = input.sliding(4).map(x => x.distinct).filter(_.length == 4).toList.head
-  val fourteenChar = input.sliding(14).map(x => x.distinct).filter(_.length == 14).toList.head
+  def distinctChar(n: Int): Int =
+    input
+      .indexOf(
+        input
+          .sliding(n)
+          .map(x => x.distinct)
+          .filter(_.length == n)
+          .toList
+          .head
+      ) + n
 
   val answer1: Int =
-    input.indexOf(fourChar) + 4
+    distinctChar(4)
 
   println(s"Answer day $day part 1: ${answer1} [${System.currentTimeMillis - start1}ms]")
 
@@ -25,6 +33,6 @@ object Day06 extends App:
     System.currentTimeMillis
 
   val answer2: Int =
-    input.indexOf(fourteenChar) + 14
+    distinctChar(14)
 
   println(s"Answer day $day part 2: ${answer2} [${System.currentTimeMillis - start2}ms]")
