018294264f43b2760e07bb5cd2ca02a0b84ff7ea
Denise Rijborz <75476995+DeniseRijborz@users.noreply.github.com>
2022-12-15 07:58:27 +0100

    Aocday03 (#3)
    * day3
    
    * completion day3
    
    * updates
    
    * update

--- aoc/src/test/scala/TestSolutions.scala
+++ aoc/src/test/scala/TestSolutions.scala
@@ -8,6 +8,11 @@ class TestSolutions extends AnyFunSuite:
   }
 
   test("Day02") {
-    assertResult(10718)(actual = Day02.answer1)
-    assertResult(14652)(actual = Day02.answer2)
+    assertResult(5)(actual = Day02.answer1)
+    assertResult(5)(actual = Day02.answer2)
+  }
+
+  test("Day03") {
+    assertResult(7766)(actual = Day02.answer1)
+    assertResult(2415)(actual = Day02.answer2)
   }
